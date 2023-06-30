3cd060660994be323f6c3ed9644f154cd9e31746
tanubhavjuneja <119427827+tanubhavjuneja@users.noreply.github.com>
2023-06-30 20:40:06 +0530

    Update Probability for Computing.py

--- Probability for Computing.py
+++ Probability for Computing.py
@@ -1,47 +1,63 @@
-import random
-import numpy as np
+from math import comb, log
 from scipy.stats import chisquare
-def generate_uniform_numbers(seed, num_numbers):
-    random.seed(seed)
-    numbers = [random.uniform(0, 1) for _ in range(num_numbers)]
-    return numbers
-def generate_normal_numbers(seed, num_numbers, mean, std_dev):
-    random.seed(seed)
-    numbers = [random.normalvariate(mean, std_dev) for _ in range(num_numbers)]
-    return numbers
-def describe_seed_effect(dist_name, seed):
-    if dist_name == generate_uniform_numbers:
-        series = dist_name(seed, num_numbers)
-        seed_effect = "The {} distribution is affected by the seed. Changing the seed will result in a different series of numbers, but the distribution properties (mean and standard deviation) will remain the same.".format(dist_name.__name__)
-    elif dist_name == generate_normal_numbers:
-        series = dist_name(seed, num_numbers, mean=0, std_dev=1)
-        seed_effect = "The {} distribution is affected by the seed. Changing the seed will result in a different series of numbers, and the distribution properties (mean and standard deviation) may also change.".format(dist_name.__name__)
-    return series, seed_effect
-def test_randomness(series):
-    observed, _ = np.histogram(series, bins='auto')
-    expected = np.full_like(observed, len(series) / len(observed))
-    observed_sum = observed.sum().astype(float)
-    expected_sum = expected.sum().astype(float)
-    observed = observed.astype(float)
-    observed *= expected_sum / observed_sum
-    _, p_value = chisquare(observed, f_exp=expected)
-    return p_value
-seed1 = 123
-seed2 = 456
-num_numbers = 1000
-uniform_numbers = generate_uniform_numbers(seed1, num_numbers)
-normal_numbers = generate_normal_numbers(seed2, num_numbers, mean=0, std_dev=1)
-uniform_cycle_length = len(set(uniform_numbers))
-normal_cycle_length = len(set(normal_numbers))
-uniform_series, uniform_effect = describe_seed_effect(generate_uniform_numbers, seed1)
-normal_series, normal_effect = describe_seed_effect(generate_normal_numbers, seed2)
-uniform_p_value = test_randomness(uniform_series)
-normal_p_value = test_randomness(normal_series)
-print("Uniform Numbers:", uniform_numbers)
-print("Normal Numbers:", normal_numbers)
-print("Uniform Cycle Length:", uniform_cycle_length)
-print("Normal Cycle Length:", normal_cycle_length)
-print("Uniform Effect of Seed:", uniform_effect)
-print("Normal Effect of Seed:", normal_effect)
-print("Uniform Chi-squared p-value:", uniform_p_value)
-print("Normal Chi-squared p-value:", normal_p_value)
+class RandomGenerator:
+    def __init__(self, seed):
+        self.seed = seed
+        self.a = 1103515245
+        self.c = 12345
+        self.m = 2 ** 31
+    def generate_uniform(self, n):
+        series = []
+        x = self.seed
+        for _ in range(n):
+            x = (self.a * x + self.c) % self.m
+            series.append(x / self.m)
+        return series
+    def generate_binomial(self, n, p):
+        uniform_numbers = self.generate_uniform(n)
+        binomial_numbers = []
+        for u in uniform_numbers:
+            num_success = sum(u > sum(comb(n, k) * p**k * (1 - p)**(n - k) for k in range(i + 1)) for i in range(n))
+            binomial_numbers.append(num_success)
+        return binomial_numbers
+    def generate_exponential(self, n, rate):
+        uniform_numbers = self.generate_uniform(n)
+        exponential_numbers = []
+        for u in uniform_numbers:
+            num = -log(1 - u) / rate
+            exponential_numbers.append(num)
+        return exponential_numbers
+    def calculate_cycle_length(self, series):
+        seen = {}
+        for i, num in enumerate(series):
+            if num in seen:
+                return i - seen[num]
+            seen[num] = i
+        return None
+    def test_randomness(self, series):
+        observed_freq = [series.count(x) for x in set(series)]
+        expected_freq = [len(series) / len(set(series))] * len(set(series))
+        chi2, p_value = chisquare(observed_freq, expected_freq)
+        return chi2, p_value
+seeds = []
+seed_no = int(input("Enter the number of seeds you want to enter: "))
+for i in range(seed_no):
+    x = int(input("Enter Seed: "))
+    seeds.append(x)
+n_trials = int(input("Enter the number of trials: "))
+p_success = float(input("Enter the probability of success for the binomial distribution (between 0 and 1): "))
+rate = float(input("Enter the rate parameter for the exponential distribution: "))
+for seed in seeds:  
+    rg = RandomGenerator(seed)
+    binomial_numbers = rg.generate_binomial(n_trials, p_success)
+    exponential_numbers = rg.generate_exponential(n_trials, rate)
+    binomial_cycle_length = rg.calculate_cycle_length(binomial_numbers)
+    exponential_cycle_length = rg.calculate_cycle_length(exponential_numbers)
+    binomial_chi2, binomial_p_value = rg.test_randomness(binomial_numbers)
+    exponential_chi2, exponential_p_value = rg.test_randomness(exponential_numbers)
+    print("Binomial Random Numbers:", binomial_numbers)
+    print("Binomial Cycle Length:", binomial_cycle_length)
+    print("Binomial Chi-squared Test: chi2 =", binomial_chi2, "p-value =", binomial_p_value)
+    print("Exponential Random Numbers:", exponential_numbers)
+    print("Exponential Cycle Length:", exponential_cycle_length)
+    print("Exponential Chi-squared Test: chi2 =", exponential_chi2, "p-value =", exponential_p_value)
